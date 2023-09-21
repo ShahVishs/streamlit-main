54f75b5efed6e76c89364892546a7ec96ee54b36
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-09-21 16:22:48 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -158,21 +158,15 @@ st.sidebar.header("Chat Sessions")
 # Check if the user is the admin (vishakha) or not
 is_admin = st.session_state.user_name == "vishakha"
 
-# Create a dictionary to store all loaded sessions
-loaded_sessions = load_previous_sessions()
-
-# Create a dictionary to group sessions by user
+# Create a dictionary to store sessions for each user
 user_sessions = {}
 
-# Get the current user's username
-current_username = st.session_state.user_name
-
-# Iterate through sessions and group them by user
-for session_id, session_data in loaded_sessions.items():
+for session_id, session_data in st.session_state.sessions.items():
     user_name = session_data['user_name']
     chat_history = session_data['chat_history']
     user_role = session_data['user_role']
-
+    
+    # Check if the current user is an admin (user_role is 'admin') or a regular user (user_role is 'user')
     if user_name not in user_sessions:
         user_sessions[user_name] = []
 
@@ -181,14 +175,13 @@ for session_id, session_data in loaded_sessions.items():
         'chat_history': chat_history,
         'user_role': user_role
     })
-
 if is_admin:
-    # If the user is an admin (vishakha), show all sessions
+    # If the user is an admin (vishakha), show all sessions for all users
     for user_name, sessions in user_sessions.items():
         st.sidebar.subheader(f"User: {user_name}")
 
         for session in sessions:
-            formatted_session_name = f"{user_name} - {current_date} - {current_day}"
+            formatted_session_name = f"{user_name} - Session {session['session_id']}"
 
             button_key = f"session_button_{session['session_id']}"
             if st.sidebar.button(formatted_session_name, key=button_key):
@@ -200,18 +193,19 @@ if is_admin:
                 st.session_state.user_role = session['user_role']
 else:
     # If the user is not an admin, show only their own session
+    current_username = st.session_state.user_name
     if current_username:
-        st.sidebar.subheader(f"Your Session")
+        st.sidebar.subheader(f"Your Sessions")
 
-        # Display the user's session
-        formatted_session_name = f"{current_username} - {current_date} - {current_day}"
+        # Display the user's sessions
+        for session in user_sessions[current_username]:
+            formatted_session_name = f"{current_username} - Session {session['session_id']}"
 
-        if st.sidebar.button(formatted_session_name):
-            # Set the current chat history to the user's session history
-            st.session_state.chat_history = user_sessions[current_username][0]['chat_history'].copy()  # Make a copy to avoid modifying the original
-            # Update the user role to match the user's role
-            st.session_state.user_role = user_sessions[current_username][0]['user_role']
-
+            if st.sidebar.button(formatted_session_name):
+                # Set the current chat history to the selected session's chat history
+                st.session_state.chat_history = session['chat_history'].copy()  # Make a copy to avoid modifying the original
+                # Update the user role to match the session's user role
+                st.session_state.user_role = session['user_role']
 file_1 = r'dealer_1_inventry.csv'
 
 loader = CSVLoader(file_path=file_1)
