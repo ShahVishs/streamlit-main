ed9f4765abc0eff16b34de27c8d1605a7f81576b
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-09-21 15:48:45 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -175,28 +175,7 @@ for session_id, session_data in st.session_state.sessions.items():
             st.session_state.user_name = user_name
             # Update the user role to match the session's user role
             st.session_state.user_role = user_role# Display a list of past sessions in the sidebar along with a delete button
-st.sidebar.header("Chat Sessions")
 
-# Check if the user is the admin (vishakha) or not
-is_admin = st.session_state.user_name == "vishakha"
-
-for session_id, session_data in st.session_state.sessions.items():
-    user_name = session_data['user_name']
-    chat_history = session_data['chat_history']
-    user_role = session_data['user_role']
-    
-    # Check if the current user is an admin (user_role is 'admin') or a regular user (user_role is 'user')
-    if is_admin or st.session_state.user_name == user_name:
-        formatted_session_name = f"{user_name} - {session_id}"
-        
-        button_key = f"session_button_{session_id}"
-        if st.sidebar.button(formatted_session_name, key=button_key):
-            # Set the current chat history to the selected session's chat history
-            st.session_state.chat_history = chat_history
-            # Update the user name to match the session's user name
-            st.session_state.user_name = user_name
-            # Update the user role to match the session's user role
-            st.session_state.user_role = user_role
 file_1 = r'dealer_1_inventry.csv'
 
 loader = CSVLoader(file_path=file_1)
