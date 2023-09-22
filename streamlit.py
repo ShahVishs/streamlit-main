ec1cdcef9e174e1ae079c37537b86c2ff3a5f2f2
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-09-22 10:56:49 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -32,10 +32,10 @@ from langchain.prompts import MessagesPlaceholder
 from langchain.agents import AgentExecutor
 import json
 
-hide_github_icon = “”"
-
+hide_github_icon = """
 .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
-“”"
+"""
+
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
