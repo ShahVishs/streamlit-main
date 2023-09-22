250ce13ae976aa6866ed10cc7cc8f44216fb2307
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-09-22 12:40:39 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -125,9 +125,6 @@ current_day = days[day_of_week]
 if 'user_name' not in st.session_state:
     st.session_state.user_name = None
 
-if 'user_sessions' not in st.session_state:
-    st.session_state.user_sessions = {}
-    
 # Define roles (e.g., 'admin' and 'user')
 ROLES = ['admin', 'user']
 
@@ -254,50 +251,19 @@ if is_admin:
                 st.session_state.user_role = session['user_role']
 else:
     # If the user is not an admin, show only their own session
-    # Check if the user is not an admin, show only their own session
-    # Check if the user is not an admin, show only their own session
     current_username = st.session_state.user_name
-    
     if current_username:
-        if current_username in st.session_state.user_sessions:
-            user_sessions = st.session_state.user_sessions[current_username]
-    
-            # Display the user's sessions
-            for session in user_sessions:
-                formatted_session_name = f"{current_username} - {session['session_id']}"
-    
-                if st.sidebar.button(formatted_session_name):
-                    # Set the current chat history to the selected session's chat history
-                    st.session_state.chat_history = session['chat_history'].copy()
-                    # Update the user role to match the session's user role
-                    st.session_state.user_role = session['user_role']
-        else:
-            st.warning("No sessions found for the current user.")
-    
-            # Create a new session for the current user
-            new_session_id = "new_session"  # You can generate a unique session ID here
-            new_session = {
-                'session_id': new_session_id,
-                'chat_history': [],
-                'user_role': 'user',  # Set the user role for the new session
-            }
-    
-            # Add the new session to the user's sessions
-            st.session_state.user_sessions.setdefault(current_username, []).append(new_session)
-    
-            # Set the current chat history to the new session's chat history
-            st.session_state.chat_history = new_session['chat_history'].copy()
-            # Update the user role to match the new session's user role
-            st.session_state.user_role = new_session['user_role']
-    
-            # Format the session name for the new session
-            formatted_session_name = f"{current_username} - {new_session_id}"
-    
+        # st.sidebar.subheader(f"Your Sessions")
+
+        # Display the user's sessions
+        for session in user_sessions[current_username]:
+            formatted_session_name = f"{current_username} - {session['session_id']}"
+
             if st.sidebar.button(formatted_session_name):
-                # Set the current chat history to the new session's chat history
-                st.session_state.chat_history = new_session['chat_history'].copy()
-                # Update the user role to match the new session's user role
-                st.session_state.user_role = new_session['user_role']
+                # Set the current chat history to the selected session's chat history
+                st.session_state.chat_history = session['chat_history'].copy()  # Make a copy to avoid modifying the original
+                # Update the user role to match the session's user role
+                st.session_state.user_role = session['user_role']
 file_1 = r'dealer_1_inventry.csv'
 
 loader = CSVLoader(file_path=file_1)
@@ -329,7 +295,7 @@ os.environ["AIRTABLE_API_KEY"] = airtable_api_key
 AIRTABLE_BASE_ID = "appAVFD4iKFkBm49q"  
 AIRTABLE_TABLE_NAME = "Question_Answer_Data" 
 # Streamlit UI setup
-st.info(" Introducing **Otto**, your cutting-edge partner in streamlining dealership and customer-related operations. At EngagedAi, we specialize in harnessing the power of automation to revolutionize the way dealerships and customers interact. Our advanced solutions seamlessly handle tasks, from managing inventory and customer inquiries to optimizing sales processes, all while enhancing customer satisfaction. Discover a new era of efficiency and convenience with us as your trusted automation ally. [engagedai.io](https://funnelai.com/). For this demo application, we will use the Inventory Dataset. Please explore it [here](https://github.com/ShahVishs/workflow/blob/main/2013_Inventory.csv) to get a sense for what questions you can ask.")
+st.info(" We're developing cutting-edge conversational AI solutions tailored for automotive retail, aiming to provide advanced products and support. As part of our progress, we're establishing a environment to check offerings and also check Our website [engane.ai](https://funnelai.com/). This test application answers about Inventry, Business details, Financing and Discounts and Offers related questions. [here](https://github.com/buravelliprasad/streamlit/blob/main/dealer_1_inventry.csv) is a inventry dataset explore and play with the data. Appointment dataset [here](https://github.com/buravelliprasad/streamlit_dynamic_retrieval/blob/main/appointment.csv)")
 # Initialize session state
 if 'chat_history' not in st.session_state:
     st.session_state.chat_history = []
@@ -353,10 +319,6 @@ else:
     # Initialize st.session_state.new_session as True for new users (excluding vishakha)
     if 'new_session' not in st.session_state and st.session_state.user_name != "vishakha":
         st.session_state.new_session = True
-     # Initialize st.session_state.chat_history for new users
-    if 'chat_history' not in st.session_state:
-        st.session_state.chat_history = []
-
     llm = ChatOpenAI(model="gpt-3.5-turbo-16k", temperature = 0)
     langchain.debug=True
     memory_key = "history"
