686eb8082e88d929504ea2241e67eae36094e4cc
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-09-26 12:18:31 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -529,8 +529,10 @@ else:
                 st.image("icon-1024.png", width=40)
             with col2:
                 st.markdown(
-                f'<div style="background-color: #F5F5F5; border-radius: 10px; padding: 10px; width: 50%; position: relative; border: 2px solid #F5F5F5;">'
-                f'<div style="position: absolute; top: 0; right: 0; transform: translate(50%, -50%); background-color: #FFD700; width: 20px; height: 20px; border-radius: 50%;"></div>'
+                f'<div style="background-color: #F5F5F5; border-radius: 10px; padding: 10px; width: 50%;'
+                f' border-top-right-radius: 10px; border-bottom-right-radius: 10px;'
+                f' border-top-left-radius: 0; border-bottom-left-radius: 0;'
+                f' border: 2px solid #F5F5F5; box-shadow: 2px 2px 5px #888888;">'
                 f'<span style="font-family: Arial, sans-serif; font-size: 16px; white-space: pre-wrap;">{answer}</span>'
                 f'</div>',
                 unsafe_allow_html=True
