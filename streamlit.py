9e83a4e3a3c322a1719f7034b5f22f13953ef100
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-09-26 13:48:24 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -515,32 +515,31 @@ else:
             user_name = st.session_state.user_name
             # message(query, is_user=True, key=f"{i}_user", avatar_style="icons", seed=6)
             # Display the answer without avatars
-            col1, col2 = st.columns([10, 0.7])  # Adjust the ratio as needed
+            col1, col2 = st.columns([0.7, 10])  # Adjust the ratio as needed
             with col1:
-                st.markdown(
-                    f'<div style="background-color: #F5F5F5; border-radius: 10px; padding: 10px; width: 50%;'
-                    f' border-top-right-radius: 0; border-bottom-right-radius: 0;'
-                    f' border-top-left-radius: 10px; border-bottom-left-radius: 10px; box-shadow: 2px 2px 5px #888888;">'
-                    f'<span style="font-family: Arial, sans-serif; font-size: 16px; white-space: pre-wrap;">{answer}</span>'
-                    f'</div>',
-                    unsafe_allow_html=True
-                )
+                st.image("icons8-user-96.png", width=50)
             with col2:
-                st.image("icon-1024.png", width=50)
-                
-            # Display the query on the right side
-            col1, col2 = st.columns([10, 0.7])  # Adjust the ratio as needed
-            with col1:
                 st.markdown(
                     f'<div style="background-color: #F5F5F5; border-radius: 10px; padding: 10px; width: 50%;'
-                    f' border-top-right-radius: 0; border-bottom-right-radius: 0;'
-                    f' border-top-left-radius: 10px; border-bottom-left-radius: 10px; box-shadow: 2px 2px 5px #888888;">'
+                    f' border-top-right-radius: 10px; border-bottom-right-radius: 10px;'
+                    f' border-top-left-radius: 0; border-bottom-left-radius: 0; box-shadow: 2px 2px 5px #888888;">'
                     f'<span style="font-family: Arial, sans-serif; font-size: 16px; white-space: pre-wrap;">{query}</span>'
                     f'</div>',
                     unsafe_allow_html=True
                 )
+                
+            col1, col2 = st.columns([0.7, 10])  # Adjust the ratio as needed
+            with col1:
+                st.image("icon-1024.png", width=50)
             with col2:
-                st.image("icons8-user-96", width=50)
+                st.markdown(
+                    f'<div style="background-color: #F5F5F5; border-radius: 10px; padding: 10px; width: 50%;'
+                    f' border-top-right-radius: 10px; border-bottom-right-radius: 10px;'
+                    f' border-top-left-radius: 0; border-bottom-left-radius: 0; box-shadow: 2px 2px 5px #888888;">'
+                    f'<span style="font-family: Arial, sans-serif; font-size: 16px; white-space: pre-wrap;">{answer}</span>'
+                    f'</div>',
+                    unsafe_allow_html=True
+                )
             # Display the logo image
             # st.image("icon-1024.png", width=40)
         
