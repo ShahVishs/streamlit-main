ab05a5e7fb6b2edcfc4a2e9038b080febf0e183d
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-09-26 14:06:59 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -514,6 +514,7 @@ else:
         for i, (query, answer) in enumerate(st.session_state.chat_history):
             user_name = st.session_state.user_name
             # message(query, is_user=True, key=f"{i}_user", avatar_style="icons", seed=6)
+                    # Display the user icon and query on the right
             col1, col2 = st.columns([1, 10])  # Adjust the ratio as needed
             with col1:
                 st.image("icons8-user-96.png", width=50)
@@ -527,13 +528,13 @@ else:
                     unsafe_allow_html=True
                 )
     
-            # Display the answer on the left
+            # Display the answer icon and response on the right
             col3, col4 = st.columns([10, 1])  # Adjust the ratio as needed
             with col3:
                 st.markdown(
                     f'<div style="background-color: #F5F5F5; border-radius: 10px; padding: 10px; width: 50%;'
-                    f' border-top-right-radius: 0; border-bottom-right-radius: 0;'
-                    f' border-top-left-radius: 10px; border-bottom-left-radius: 10px; box-shadow: 2px 2px 5px #888888;">'
+                    f' border-top-right-radius: 10px; border-bottom-right-radius: 10px;'
+                    f' border-top-left-radius: 0; border-bottom-left-radius: 0; box-shadow: 2px 2px 5px #888888;">'
                     f'<span style="font-family: Arial, sans-serif; font-size: 16px; white-space: pre-wrap;">{answer}</span>'
                     f'</div>',
                     unsafe_allow_html=True
