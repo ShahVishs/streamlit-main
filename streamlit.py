bda452a672590214f707d269ba8e03e69563c2c0
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-09-26 14:17:58 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -530,7 +530,7 @@ else:
                 )
     
             # Display the answer icon and response on the right
-            col3, col4 = st.columns([5, 0.5])  # Adjust the ratio as needed
+            col3, col4 = st.columns([2, 0.5])  # Adjust the ratio as needed
             with col3:
                 st.markdown(
                     f'<div style="background-color: #F5F5F5; border-radius: 10px; padding: 10px; width: 90%;'
