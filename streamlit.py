c0ce87026abdb6473ccb8498d145cc3da04f465b
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-09-26 11:11:58 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -518,20 +518,20 @@ else:
             # st.image("icon-1024.png", width=40)
         
             # Display the answer with the desired avatar style
-            # message(answer, key=f"{i}_answer", avatar_style="initials", seed="AI",)
+            message(answer, key=f"{i}_answer", avatar_style="initials", seed="AI",)
             # Display the logo image
-            st.image("icon-1024.png", width=40)
+            # st.image("icon-1024.png", width=40)
             
-            # Add a bit more space between the query and answer messages
-            st.markdown('<div style="height: 10px;"></div>', unsafe_allow_html=True)
+            # # Add a bit more space between the query and answer messages
+            # st.markdown('<div style="height: 10px;"></div>', unsafe_allow_html=True)
             
-            # Wrap the answer in a div with background color and padding
-            st.markdown(
-                f'<div style="background-color: #e0e0e0; border-radius: 5px; padding: 10px;">'
-                f'{answer}'
-                f'</div>',
-                unsafe_allow_html=True
-            )
+            # # Wrap the answer in a div with background color and padding
+            # st.markdown(
+            #     f'<div style="background-color: #e0e0e0; border-radius: 5px; padding: 10px;">'
+            #     f'{answer}'
+            #     f'</div>',
+            #     unsafe_allow_html=True
+            # )
     if st.session_state.user_name and st.session_state.chat_history:
         try:
             save_chat_to_airtable(st.session_state.user_name, user_input, output)
