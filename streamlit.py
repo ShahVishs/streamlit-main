3b78e359f8a71274b8bbcbfbaada68f4774025c0
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-09-26 15:40:23 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -351,7 +351,7 @@ else:
     output = ""
     
     if st.session_state.user_name is None:
-        user_name = st.text_input("Your name:", key='user_name_input')
+        user_name = st.text_input("Your name:")
         if user_name:
             st.session_state.user_name = user_name
         if user_name == "vishakha":
