62fe23fb145a01108c254ac2a5689c1d25ac1574
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-09-26 12:53:31 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -524,7 +524,7 @@ else:
             # st.image("icon-1024.png", width=40)
     
             # Display the answer without avatars
-            col1, col2 = st.columns([0.6, 10])  # Adjust the ratio as needed
+            col1, col2 = st.columns([0.7, 10])  # Adjust the ratio as needed
             with col1:
                 st.image("icon-1024.png", width=50)
             with col2:
