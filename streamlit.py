13eecdaa0864f927eae54f82f3526c089af484ae
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-09-27 12:59:57 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -426,10 +426,10 @@ def main():
                 except Exception as e:
                     st.error(f"An error occurred: {e}")
 if __name__ == "__main__":
-    profiler = cProfile.Profile()
-    profiler.enable()
+    # profiler = cProfile.Profile()
+    # profiler.enable()
 
     main()
 
-    profiler.disable()
-    profiler.print_stats(sort='cumtime')
+    # profiler.disable()
+    # profiler.print_stats(sort='cumtime')
