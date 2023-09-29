bb3ead07b0504747663f79270985352e354a066c
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-09-29 18:58:20 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -183,8 +183,7 @@ if __name__ == "__main__":
     df = pd.read_csv("appointment_new.csv")
     input_template = template.format(dhead=df.head().to_markdown(), details=details)
 
-system_message = SystemMessage(
-        content=input_template)  # Corrected variable name here
+system_message = SystemMessage(content=input_template)  # Corrected variable name here
 
 prompt = OpenAIFunctionsAgent.create_prompt(
         system_message=system_message,
