1d12b02871b2ea1dced79654a5bca7a190a60162
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-09-29 14:07:19 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -177,17 +177,20 @@ if __name__ == "__main__":
     df = pd.read_csv("appointment_new.csv")
     input_templete = template.format(dhead=df.head().to_markdown(),details=details)
 
-
 system_message = SystemMessage(
-        content=input_templete)
+    content=input_templete)
 
 prompt = OpenAIFunctionsAgent.create_prompt(
-        system_message=system_message,
-        extra_prompt_messages=[MessagesPlaceholder(variable_name=memory_key)]
-    )
+    system_message=system_message,
+    extra_prompt_messages=[MessagesPlaceholder(variable_name=memory_key)]
+)
 
-repl = PythonAstREPLTool(locals={"df": df}, name="python_repl",
-        description="Use to check on available appointment times for a given date and time. The input to this tool should be a string in this format mm/dd/yy. This is the only way for you to answer questions about available appointments. This tool will reply with available times for the specified date in 24hour time, for example: 15:00 and 3pm are the same.",args_schema=PythonInputs)
+repl = PythonAstREPLTool(
+    locals={"df": df},
+    name="python_repl",
+    description="Use to check on available appointment times for a given date and time. The input to this tool should be a string in this format mm/dd/yy. This is the only way for you to answer questions about available appointments. This tool will reply with available times for the specified date in 24hour time, for example: 15:00 and 3pm are the same.",
+    args_schema=PythonInputs
+)
 tools = [tool1,repl,tool3]
 
 agent = OpenAIFunctionsAgent(llm=llm, tools=tools, prompt=prompt)
