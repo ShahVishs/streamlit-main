c4d5bdab55a88183d5932972ded37b8746a7c66a
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-09-29 15:07:41 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -185,9 +185,11 @@ prompt = OpenAIFunctionsAgent.create_prompt(
         system_message=system_message,
         extra_prompt_messages=[MessagesPlaceholder(variable_name=memory_key)]
     )
-
-repl = PythonAstREPLTool(locals={"df": df}, name="python_repl",
+try:
+	repl = PythonAstREPLTool(locals={"df": df}, name="python_repl",
         description="Use to check on available appointment times for a given date and time. The input to this tool should be a string in this format mm/dd/yy. This is the only way for you to answer questions about available appointments. This tool will reply with available times for the specified date in 24hour time, for example: 15:00 and 3pm are the same.",args_schema=PythonInputs)
+except Exception as e:
+	st.error(f"An error occurred while running pythonrepl: {e}")
 tools = [tool1,repl,tool3]
 
 agent = OpenAIFunctionsAgent(llm=llm, tools=tools, prompt=prompt)
