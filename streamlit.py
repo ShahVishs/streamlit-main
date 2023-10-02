97c91e317039e813f5c33eee64134ac93d31d3bc
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-10-02 15:43:58 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,5 @@
+from pydantic import BaseModel, Field
+from langchain.tools import PythonAstREPLTool
 import os
 import streamlit as st
 from airtable import Airtable
@@ -31,9 +33,8 @@ from langchain.schema.messages import SystemMessage
 from langchain.prompts import MessagesPlaceholder
 from langchain.agents import AgentExecutor
 from langchain.smith import RunEvalConfig, run_on_dataset
-from pydantic import BaseModel, Field
+
 import pandas as pd
-from langchain.tools import PythonAstREPLTool
 
 pd.set_option('display.max_rows', 20)
 pd.set_option('display.max_columns', 20)
