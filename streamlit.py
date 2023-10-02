b6728f30e654c2cb3ff0e524e368a03a6e2f54e1
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-10-02 13:39:06 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -178,7 +178,7 @@ if __name__ == "__main__":
 
    
     # Define the args_schema as a dictionary
-    args_schema = PythonInputs(query="your_initial_query_here")
+    args_schema = PythonInputs(query=None)
        
 	
     # Create the PythonAstREPLTool with the args_schema
